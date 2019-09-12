866d901d6c33675ec5eb67a6db3492dc5edd3c8c
damionjerrod <damion.dawson@gmail.com>
2019-09-12 03:03:20 -0400

    Exercise 3: Functions

---
+++ main3.js
@@ -0,0 +1,24 @@
+//function that adds two arguments.
+var totalOne = functionOne(6, 6);
+function functionOne(num1, num1) {
+    return num1 + num1;
+}
+
+console.log(totalOne);
+
+
+//function that multiplies two arguments.
+var totalTwo = functionTwo(6, 6);
+function functionTwo(num2, num2) {
+    return num2 * num2;
+}
+
+console.log(totalTwo);
+
+
+//couldn't figure this out.
+var startFinish = functionThree(2, 10);
+function functionThree(start, finish) {
+    while(start == 2 && finish <= 10){
+        console.log(startFinish);
+        count++;
