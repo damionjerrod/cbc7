ec378c6f0cf28463436c28070c80b84b426a828d
damionjerrod <damion.dawson@gmail.com>
2019-09-20 00:03:23 -0400

    AutoComplete Exercise: Missing Validation

---
+++ AutoComp/Main.js
@@ -0,0 +1,48 @@
+/*create an auto complete feature
+1. input text box
+2. list of terms
+3. compare
+4. validate input (no special characters)
+*/
+
+function autocomplete(inp, arr){
+    var currentFocus;
+    inp.addEventListener("input", function(e){
+        var a, b, i, val = this.value;
+        closeAllLists();
+        if (!val) {return false;}
+        currentFocus = -1;
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        this.parentNode.appendChild(a);
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                b = document.createElement("DIV");
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                b.addEventListener("click", function(e) {
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+    });
+    function closeAllLists(elmnt) {
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i=0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+
+var animals = ["cats", "dogs", "penguins"];
+
+autocomplete(document.getElementById("theInput"), animals);
